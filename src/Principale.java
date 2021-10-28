459bc256ba0376dd8dedccd2479a875e0ce84554
JasonKing2050 <le_blanc.jean_francois@courrier.uqam.ca>
2021-10-27 23:19:45 -0400

    Refactoring

--- src/Principale.java
+++ src/Principale.java
@@ -4,17 +4,11 @@ public class Principale {
         Forme3D F3_1 = new CylindreDroit(F2_1,3.23);
         Forme3D F3_2 = new Sphere(3.0);
 
-
-
         System.out.println(doubleAFormatSortie2D(F2_1.aire()));
         System.out.println(doubleAFormatSortie3D(F3_1.Volume()));
         System.out.println(doubleAFormatSortie3D(F3_2.Volume()));
 
-
-
     }
-
-
 
     private static String doubleAFormatSortie2D(double montantSansFormat) {
         return String.format("L'aire est de : %.2f m2", montantSansFormat);
